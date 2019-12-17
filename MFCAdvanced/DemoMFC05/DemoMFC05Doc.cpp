503371cf919fcd599b5f2d6489d2349222e834ca
lianxiang <1161851688@qq.com>
2019-12-18 02:33:20 +0800

    文本编辑器

---
+++ MFCAdvanced/DemoMFC05/DemoMFC05Doc.cpp
@@ -0,0 +1,137 @@
+﻿
+// DemoMFC05Doc.cpp: CDemoMFC05Doc 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "DemoMFC05.h"
+#endif
+
+#include "DemoMFC05Doc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CDemoMFC05Doc
+
+IMPLEMENT_DYNCREATE(CDemoMFC05Doc, CDocument)
+
+BEGIN_MESSAGE_MAP(CDemoMFC05Doc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CDemoMFC05Doc 构造/析构
+
+CDemoMFC05Doc::CDemoMFC05Doc() noexcept
+{
+	// TODO: 在此添加一次性构造代码
+
+}
+
+CDemoMFC05Doc::~CDemoMFC05Doc()
+{
+}
+
+BOOL CDemoMFC05Doc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 在此添加重新初始化代码
+	// (SDI 文档将重用该文档)
+
+	return TRUE;
+}
+
+
+
+
+// CDemoMFC05Doc 序列化
+
+void CDemoMFC05Doc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 缩略图的支持
+void CDemoMFC05Doc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 修改此代码以绘制文档数据
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 搜索处理程序的支持
+void CDemoMFC05Doc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 从文档数据设置搜索内容。
+	// 内容部分应由“;”分隔
+
+	// 例如:     strSearchContent = _T("point;rectangle;circle;ole object;")；
+	SetSearchContent(strSearchContent);
+}
+
+void CDemoMFC05Doc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CDemoMFC05Doc 诊断
+
+#ifdef _DEBUG
+void CDemoMFC05Doc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CDemoMFC05Doc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CDemoMFC05Doc 命令
---
+++ MFCAdvanced/DemoMFC05/DemoMFC05View.cpp
@@ -0,0 +1,178 @@
+﻿
+// DemoMFC05View.cpp: CDemoMFC05View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "DemoMFC05.h"
+#endif
+
+#include "DemoMFC05Doc.h"
+#include "DemoMFC05View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CDemoMFC05View
+
+IMPLEMENT_DYNCREATE(CDemoMFC05View, CView)
+
+BEGIN_MESSAGE_MAP(CDemoMFC05View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_WM_CREATE()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_CHAR()
+END_MESSAGE_MAP()
+
+// CDemoMFC05View 构造/析构
+
+CDemoMFC05View::CDemoMFC05View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CDemoMFC05View::~CDemoMFC05View()
+{
+}
+
+BOOL CDemoMFC05View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CDemoMFC05View 绘图
+
+void CDemoMFC05View::OnDraw(CDC* /*pDC*/)
+{
+	CDemoMFC05Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CDemoMFC05View 打印
+
+BOOL CDemoMFC05View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CDemoMFC05View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CDemoMFC05View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CDemoMFC05View 诊断
+
+#ifdef _DEBUG
+void CDemoMFC05View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CDemoMFC05View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CDemoMFC05Doc* CDemoMFC05View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CDemoMFC05Doc)));
+	return (CDemoMFC05Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CDemoMFC05View 消息处理程序
+
+//窗口创建后自动调用
+int CDemoMFC05View::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CView::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	// TODO:  在此添加您专用的创建代码
+	//获取字体信息
+	CClientDC dc(this);
+	TEXTMETRIC tm;//字体信息结构体
+	dc.GetTextMetrics(&tm);
+
+	//设置宽度 高度
+	CreateSolidCaret(tm.tmAveCharWidth/8, tm.tmHeight);//创建插入符
+
+	ShowCaret();//显示
+
+	return 0;
+}
+
+
+void CDemoMFC05View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	m_point = point;
+
+	SetCaretPos(point);
+
+	m_str = TEXT("");
+	m_str.Empty();
+
+	CView::OnLButtonDown(nFlags, point);
+}
+
+//点击键盘，启动调用
+void CDemoMFC05View::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CClientDC dc(this);
+
+	if (nChar == VK_RETURN) {//换行
+		m_str.Empty();
+		TEXTMETRIC tm;//字体信息结构体
+		dc.GetTextMetrics(&tm);//获取字体信息
+		m_point.y = m_point.y + tm.tmHeight;
+	}
+	else if (nChar == VK_BACK) {//退格
+		//获取背景色
+		COLORREF color = dc.GetBkColor();
+		//设置字体的颜色
+		COLORREF oldColor = dc.SetTextColor(color);
+		//白色重新写一次
+		dc.TextOut(m_point.x, m_point.y, m_str);
+		//去掉最后一个字符
+		m_str = m_str.Left(m_str.GetLength() - 1);
+		//恢复颜色
+		dc.SetTextColor(oldColor);
+	}
+	else {
+		m_str += (TCHAR)nChar;
+	}
+
+	CSize size = dc.GetTextExtent(m_str);//获取字符串长度
+	int x = m_point.x + size.cx;
+	int y = m_point.y;
+	SetCaretPos(CPoint(x, y));
+	dc.TextOut(m_point.x, m_point.y, m_str);
+	CView::OnChar(nChar, nRepCnt, nFlags);
+}
